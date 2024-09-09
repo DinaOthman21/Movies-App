30a7c92d3ea85e2614174d37781716b34077cf43
dina othman <dinaosman581@gmail.com>
2024-09-09 14:04:35 +0300

    add BottomNavigationBar

---
+++ app/src/main/java/com/example/films_app/movieList/presentation/common/BottomNavigationBar.kt
@@ -0,0 +1,79 @@
+package com.example.films_app.movieList.presentation.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Movie
+import androidx.compose.material.icons.rounded.Upcoming
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import com.example.films_app.R
+import com.example.films_app.movieList.presentation.moviesList.MovieListUIEvent
+import com.example.films_app.movieList.presentation.navigation.Screen
+
+
+@Composable
+fun BottomNavigationBar(
+    bottomNavController : NavHostController,
+    onEvent :(MovieListUIEvent) -> Unit
+){
+
+    val items = listOf(
+        BottomItem( title = stringResource(id = R.string.popular) , icon = Icons.Rounded.Movie) ,
+        BottomItem(  title = stringResource(id = R.string.upcoming) , icon = Icons.Rounded.Upcoming )
+    )
+
+    val selected = rememberSaveable {
+        mutableIntStateOf(0)
+    }
+
+    NavigationBar {
+        Row(
+            modifier = Modifier.background(MaterialTheme.colorScheme.inverseOnSurface)
+        ) {
+            items.forEachIndexed { index, bottomItem ->
+                NavigationBarItem(
+                    selected = selected.intValue ==index,
+                    onClick = {
+                        selected.intValue=index
+                        when(selected.intValue){
+                            0->{
+                                onEvent(MovieListUIEvent.Navigate)
+                                bottomNavController.popBackStack()
+                                bottomNavController.navigate(Screen.PopularMovies.route)
+                            }
+                            1->{
+                                onEvent(MovieListUIEvent.Navigate)
+                                bottomNavController.popBackStack()
+                                bottomNavController.navigate(Screen.UpComingMovies.route)
+                            }
+                        }
+                    },
+                    icon = {
+                        Icon(
+                            imageVector = bottomItem.icon,
+                            contentDescription = bottomItem.title ,
+                            tint = MaterialTheme.colorScheme.onBackground
+                        )
+                    } ,
+                    label = {
+                        Text(
+                            text = bottomItem.title,
+                            color = MaterialTheme.colorScheme.onBackground
+                        )
+                    }
+                )
+            }
+
+        }
+    }
+}
--- app/src/main/java/com/example/films_app/movieList/presentation/moviesList/HomeScreen.kt
+++ app/src/main/java/com/example/films_app/movieList/presentation/moviesList/HomeScreen.kt
@@ -1,27 +1,18 @@
 package com.example.films_app.movieList.presentation.moviesList
 
 
-import androidx.compose.foundation.background
+
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Movie
-import androidx.compose.material.icons.rounded.Upcoming
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.stringResource
@@ -32,7 +23,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.films_app.R
-import com.example.films_app.movieList.presentation.common.BottomItem
+import com.example.films_app.movieList.presentation.common.BottomNavigationBar
 import com.example.films_app.movieList.presentation.navigation.Screen
 
 
@@ -44,7 +35,6 @@ fun HomeScreen(
     movieListViewModel : MovieListViewModel
 ){
 
-  // val movieListViewModel = hiltViewModel<MovieListViewModel>()
     val movieState =movieListViewModel.movieListState.collectAsState().value
     val bottomNavController = rememberNavController()
 
@@ -87,62 +77,5 @@ fun HomeScreen(
 
 }
 
-
-@Composable
-fun BottomNavigationBar(
-    bottomNavController : NavHostController ,
-    onEvent :(MovieListUIEvent) -> Unit
-){
-
-    val items = listOf(
-        BottomItem( title = stringResource(id = R.string.popular) , icon = Icons.Rounded.Movie) ,
-        BottomItem(  title = stringResource(id = R.string.upcoming) , icon = Icons.Rounded.Upcoming )
-    )
-
-    val selected = rememberSaveable {
-        mutableIntStateOf(0)
-    }
-
-    NavigationBar {
-        Row(
-            modifier = Modifier.background(MaterialTheme.colorScheme.inverseOnSurface)
-        ) {
-            items.forEachIndexed { index, bottomItem ->
-                NavigationBarItem(
-                    selected = selected.intValue ==index,
-                    onClick = {
-                        selected.intValue=index
-                        when(selected.intValue){
-                            0->{
-                                onEvent(MovieListUIEvent.Navigate)
-                                bottomNavController.popBackStack()
-                                bottomNavController.navigate(Screen.PopularMovies.route)
-                            }
-                            1->{
-                                onEvent(MovieListUIEvent.Navigate)
-                                bottomNavController.popBackStack()
-                                bottomNavController.navigate(Screen.UpComingMovies.route)
-                            }
-                        }
-                    },
-                    icon = {
-                        Icon(
-                        imageVector = bottomItem.icon,
-                        contentDescription = bottomItem.title ,
-                            tint = MaterialTheme.colorScheme.onBackground
-                        )
-                    } ,
-                    label = {
-                        Text(
-                            text = bottomItem.title,
-                            color = MaterialTheme.colorScheme.onBackground
-                            )
-                    }
-                )
-            }
-
-        }
-    }
-}
 
 
